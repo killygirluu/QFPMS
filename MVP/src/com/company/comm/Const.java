92a8de036eb099e789e4a21b5fc4eac61959f3a2
killygirluu <18700585736@163.com>
2021-12-16 13:01:21 +0800

    origin v0.0.0

---
+++ MVP/src/com/company/comm/Const.java
@@ -0,0 +1,11 @@
+package com.company.comm;
+
+public interface Const {
+    String SUCCESS = "success";
+    String ERROR = "error";
+    String CURRENT_USER = "current_user";
+    String ERROR_MSG = "errMsg";
+    String LOGIN = "LOGIN";
+    String USER = "USER";
+    int DEFAULT_PAGE_SIZE = 5;
+}
